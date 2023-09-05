2c6ea4cea33e169c98c6a21550bfd1ddf0ced79b
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2023-09-05 15:59:41 +0530

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -20,22 +20,32 @@ const Header = () => {
               id="search_field"
               className="form-control"
               placeholder="Search Your Favourite Restaurant...."
-              
             />
 
             <div className="input-group-append">
               <button id="search_btn" className="btn">
-              <i class="fa fa-search" aria-hidden="true"></i>
+                <i class="fa fa-search" aria-hidden="true"></i>
               </button>
             </div>
-
           </div>
         </div>
 
+        {/* Login */}
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          <button className="btn" id="login_btn">
+            Login
+          </button>
+          <span className="ml-3" id="cart">
+            Cart{" "}
+          </span>
+          {/* margin-left 3units from left  :Bootstarp  */}
+          <span className="ml-1" id="cart_count">
+            2
+          </span>
+        </div>
       </nav>
     </>
   );
 };
 
 export default Header;
-
