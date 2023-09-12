64b01ceb995dced68552e85c0e1ec2f3644c5f09
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2023-09-12 17:42:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-
-import './App.css';
-import  Header from "./components/layout/Header";
-import Home from './components/Home';
-import Footer from './components/layout/Footer';
-
+import "./App.css";
+import Header from "./components/layout/Header";
+import Home from "./components/Home";
+import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-      <Header/>
-      <div className='container container-fluid'>
-      <Home/>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          
+          <Routes>
+            <Route path="/" element= {<Home />} exact></Route> 
+            {/* whenever "/" get rendered one has to direct it to "Home"  */}
+          </Routes>
+
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-      
-    </div>
+    </Router>
   );
-};
+}
 
 export default App;
