7664a146647dd4fb7f26ad857f12cbe9778696c1
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2023-09-12 11:28:48 +0530

    Add files via upload

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='py-1'>
+        <p className='text-center mt-2'>
+            Food Delivery Website - 2023-2024, All Rights Reserved
+        </p>
+    </div>
+      
+    </>
+  )
+}
+
+export default Footer;
