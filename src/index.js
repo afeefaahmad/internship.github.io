8747b5a34b7109deb3007b274af52b4e9702e202
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2023-09-12 17:42:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import store from "./store";
+import {Provider} from "react-redux";
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+  <App />
+  </Provider>
+  // Provider will connect redux to react & it's a component ..make one redux component(eg here store) availble to all components
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
