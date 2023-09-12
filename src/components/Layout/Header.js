bcd44d46001a9e5f545e6408209e41a225439d1c
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2023-09-12 11:28:19 +0530

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,46 +6,49 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <nav className="navbar row">
+      <nav className="navbar row sticky-top">
         {/* logo */}
         <div className="col-12 col-md-3">
-          <img src="/images/logo.webp" alt="logo" className="logo"></img>
+          <img src="/images/logo.webp" className='logo' alt="logo" ></img>
         </div>
 
         {/* search bar */}
-        <div className="col-12 col-md-6 mt-2 mt-md-0">
-          <div className="input-group">
-            <input
-              type="text"
-              id="search_field"
-              className="form-control"
-              placeholder="Search Your Favourite Restaurant...."
-            />
+<div className="col-12 col-md-6 mt-2 mt-md-0">
+<div className="input-group">
+  <input
+    type="text"
+    id="search_field"
+    className="form-control"
+    placeholder="Search Your Favourite Restaurant...."
+  />
 
-            <div className="input-group-append">
-              <button id="search_btn" className="btn">
-                <i class="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </div>
-          </div>
-        </div>
+  <div className="input-group-append">
+    <button id="search_btn" className="btn">
+      <i class="fa fa-search" aria-hidden="true"></i>
+    </button>
+  </div>
+</div>
+</div>
 
-        {/* Login */}
-        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <button className="btn" id="login_btn">
-            Login
-          </button>
-          <span className="ml-3" id="cart">
-            Cart{" "}
-          </span>
-          {/* margin-left 3units from left  :Bootstarp  */}
-          <span className="ml-1" id="cart_count">
-            2
-          </span>
-        </div>
+{/* Login */}
+<div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+<button className="btn" id="login_btn">
+  Login
+</button>
+<span className="ml-3" id="cart">
+  Cart{" "}
+</span>
+{/* margin-left 3units from left  :Bootstarp  */}
+<span className="ml-1" id="cart_count">
+  2
+</span>
+</div>
       </nav>
     </>
   );
 };
 
+
 export default Header;
+
+
