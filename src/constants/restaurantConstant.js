b16f8dda17d9922008dd7cdc1a6f2bde5ced6380
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2023-09-13 22:07:32 +0530

    Create restaurantConstant.js

---
+++ src/constants/restaurantConstant.js
@@ -0,0 +1,7 @@
+// In this file we will define action types 
+export const ALL_RESTAURANTS_REQUEST = "ALL_RESTAURANTS_REQUEST";
+export const ALL_RESTAURANTS_SUCCESS = "ALL_RESTAURANTS_SUCCESS";
+export const ALL_RESTAURANTS_FAIL = "ALL_RESTAURANTS_FAIL";
+
+// This msg will be shown to redux whenever we make any actions 
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
